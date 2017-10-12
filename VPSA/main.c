1d5b1a98b90af47e98db15947061ca776017d605
Ziga Strgar <ziga_strgar@hotmail.com>
2017-10-12 15:08:36 +0200

    Initial Commit

---
+++ VPSA/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  VPSA
+//
+//  Created by Ziga Strgar on 12/10/2017.
+//  Copyright © 2017 Ziga Strgar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
