79f517ad5102d0e88cd32cac1cb59c8577fa9e5c
Ziga Strgar <ziga_strgar@hotmail.com>
2017-11-02 13:14:01 +0100

    Predavanje 5

--- VPSA/Predavanja/Predavanje5/main.c
+++ VPSA/Predavanja/Predavanje5/main.c
@@ -1,15 +1,43 @@
-//
-//  main.c
-//  Predavanje5
-//
-//  Created by Ziga Strgar on 02/11/2017.
-//  Copyright © 2017 Ziga Strgar. All rights reserved.
-//
-
 #include <stdio.h>
+#include <omp.h>
+
+#define NTHREADS 4
+#define N 100000000
+
+double vectorA[N];
+double vectorB[N];
+double scalarProduct = 0;
+double resultVector[NTHREADS];
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    omp_set_num_threads(NTHREADS);
+    
+    for (int i = 0; i < NTHREADS; i++) {
+        resultVector[i] = 0;
+    }
+    
+    for (int i = 0; i < N; i++) {
+        vectorA[i] = 1;
+        vectorB[i] = 1;
+    }
+    
+    #pragma omp parallel for reduction(+:scalarProduct)
+    for (int i = 0; i < N; i++) {
+        //resultVector[omp_get_thread_num()] += vectorA[i] * vectorB[i];
+        scalarProduct += vectorA[i] * vectorB[i];
+    }
+    
+    /*for (int i = 0; i < NTHREADS; i++) {
+        scalarProduct += resultVector[i];
+    }*/
+    
+    printf("Skalarni produkt: %f\n", scalarProduct);
+    
+    /*#pragma omp parallel
+    {
+    printf("Pozdravljen svet, sem nit številka %d\n", omp_get_thread_num());
+    }
+     */
+    
+    
 }
